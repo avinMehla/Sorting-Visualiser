0f6b747cdc64cdff2398079f64ce01efc6797b9b
Avin Mehla <Egselfish123@gmail.com>
2025-08-25 20:46:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Sortingvisualizer from './sortingvisualizer/sortingvisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Sortingvisualizer></Sortingvisualizer>
+    </div>
+  );
+}
+
+export default App;
